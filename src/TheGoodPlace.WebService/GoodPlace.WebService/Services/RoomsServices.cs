5bbe121590cb915ef6a37f3f6e42671e3c1d0636
AntoineSmet <antoinesmetdev@mic-belgique.be>
2023-05-25 15:42:00 +0200

    refactor

--- src/TheGoodPlace.WebService/GoodPlace.WebService/Services/RoomsServices.cs
+++ src/TheGoodPlace.WebService/GoodPlace.WebService/Services/RoomsServices.cs
@@ -26,11 +26,13 @@ namespace GoodPlace.WebService.Services
     {
         private RoomsDataService _roomDataService;
         private DataService _dataService;
+        private readonly IConfiguration _configuration;
 
         public RoomsServices(DataService dataService, IConfiguration configuration)
         {
             _roomDataService = new RoomsDataService();
             _dataService = dataService;
+            _configuration = configuration;
         }
 
         public async Task<RoomRankingDto> GetRoomRanking()
@@ -52,28 +54,28 @@ namespace GoodPlace.WebService.Services
             return ranking;
         }
 
-        public string GetJustification()
-        {
-            string rankingRoom = JsonConvert.SerializeObject(this.GetRoomRanking());
-            string endpoint = "https://goodplaceopenai.openai.azure.com/";
-            string key = "182533ff6c92423985d5476424f2fe8d";
+        //public string GetJustification()
+        //{
+        //    string rankingRoom = JsonConvert.SerializeObject(this.GetRoomRanking());
+        //    string endpoint = "https://goodplaceopenai.openai.azure.com/";
+        //    string key = "182533ff6c92423985d5476424f2fe8d";
 
-            string engine = "text";
+        //    string engine = "text";
 
-            OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+        //    OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
 
-            string prompt = "En moins de 50 mots justifie moi de manière simple et scientifique pourquoi la Studio est la meilleur piece pour travailler tu peux te baser sur ce json pour y repondre sans mentionner le format json" ;
-            Console.Write($"Input: {prompt}\n");
+        //    string prompt = "En moins de 50 mots justifie moi de manière simple et scientifique pourquoi la Studio est la meilleur piece pour travailler tu peux te baser sur ce json pour y repondre sans mentionner le format json" ;
+        //    Console.Write($"Input: {prompt}\n");
 
 
-            Response<Completions> completionsResponse =
-                client.GetCompletions(engine , prompt);
-            string completion = completionsResponse.Value.Choices[0].Text;
+        //    Response<Completions> completionsResponse =
+        //        client.GetCompletions(engine , prompt);
+        //    string completion = completionsResponse.Value.Choices[0].Text;
 
-            Console.WriteLine($"Chatbot: {completion}");
+        //    Console.WriteLine($"Chatbot: {completion}");
    
-            return completion;
-        }
+        //    return completion;
+        //}
 
         //public RoomRankingWithJustificationDto GetRoomRankingWithJustification()
         //{
@@ -94,13 +96,11 @@ namespace GoodPlace.WebService.Services
         //    return rankingWithJustification;
         //}
 
-        public async Task<List<RoomEnvironnementDto>> CreateRankingFromPayloads()
+       public async Task<List<RoomEnvironnementDto>> CreateRankingFromPayloads()
         {
-            // Récupérer les salles depuis le service mock
             var rooms = _roomDataService.GetRooms().Where(x => !string.IsNullOrEmpty(x.DeviceId)).ToList();
             var datas = _dataService.GetRecentRecords(DateTime.Now.AddDays(-400));
 
-            // Associer les valeurs de chaque appareil à la salle correspondante
             string roomList = "";
             foreach (Room room in rooms)
             {
@@ -108,58 +108,27 @@ namespace GoodPlace.WebService.Services
                 roomList += $"La salle {room.Name} : température {devicePayloads.Temperature} degrés, luminosité {devicePayloads.Luminosity} lumens, humidité {devicePayloads.Humidity}%";
             }
 
-            // Ajouter le préprompt
             string prompt = $"Je souhaite réserver une salle pour une réunion. J'ai le choix entre {rooms.Count} salles : {roomList}. " +
                             $"Quelle salle me conseilles-tu et pourquoi ? Renvoie-moi le résultat sous forme d'une liste JSON respectant ce format : \"Name\" (le nom de la salle sans le mot \"salle\"), \"WellnessValue\" (en pourcentage), \"Temperature\", \"Humidity\", \"Luminosity\", \"Justification\" (en une ligne). Réponds uniquement dans le format JSON. Toutes les salles doivent être présentes dans la liste et seront triées par ordre décroissant de wellnessValue.";
 
-            // OpenAI
-            string apiKey = "";
-            string apiUrl = "";
-            string result = "";
+            string apiKey = _configuration.GetValue<string>("ConnectionStrings:OpenAiApiKey");
+            string apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-            dynamic requestBody = new
-            {
-                prompt = prompt,
-                max_tokens = 800 // Le nombre maximum de tokens à générer
-            };
-            string requestBodyJson = JsonConvert.SerializeObject(requestBody);
+            IOpenAiService openAiService = new OpenAiService(apiKey, apiUrl);
+            string result = await openAiService.GenerateTextAsync(prompt);
 
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-                var content = new StringContent(requestBodyJson, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(apiUrl, content);
-              
-                string responseJson = await response.Content.ReadAsStringAsync();
+            
 
-                JObject responseData = JObject.Parse(responseJson);
-                JToken choicesToken = responseData["choices"];
-                if (choicesToken != null && choicesToken.HasValues)
-                {
-                    result = choicesToken[0]["text"].Value<string>().Trim('\n', '"');
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.WriteLine("Une erreur s'est produite lors de la génération du texte.");
-                }
+            List<RoomEnvironnementDto> recommendationList = null;
 
-                List<RoomEnvironnementDto> recommendationList = null;
+                string jsonToDeserialize = result.Trim('.', ' ', '\n');
+                recommendationList = JsonConvert.DeserializeObject<List<RoomEnvironnementDto>>(jsonToDeserialize);
+            
 
-                try
-                {
-                    string jsonToDeserialize = result.Trim('.', ' ', '\n');
-                    recommendationList = JsonConvert.DeserializeObject<List<RoomEnvironnementDto>>(jsonToDeserialize);
-                    Console.WriteLine("hello" + recommendationList);
-                }
-                catch (JsonException jsonEx)
-                {
-                    Console.WriteLine($"Une erreur s'est produite lors de la désérialisation de la réponse JSON : {jsonEx.Message}");
-                }
-
-                return recommendationList;
-            }
+            return recommendationList;
         }
+
+
 
 
 
