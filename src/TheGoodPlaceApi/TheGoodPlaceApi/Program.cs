bf2a1e75492206d775a22612948b9828489fcff8
Antoine Smet <antoinesmetdev@gmail.com>
2023-12-05 17:02:01 +0100

    react js --> react vite ts

--- src/TheGoodPlaceApi/TheGoodPlaceApi/Program.cs
+++ src/TheGoodPlaceApi/TheGoodPlaceApi/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:3000",
-                                              "http://localhost:3001");
+                                              "http://localhost:5173");
                       });
 });
 
