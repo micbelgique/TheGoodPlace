f6f09b6f3e8f77f8d20fd184ff9e71f95ebb8fd1
Antoine Smet <antoinesmetdev@gmail.com>
2023-12-05 15:57:00 +0100

    add 2 rankings endpoint

--- src/TheGoodPlaceApi/TheGoodPlaceApi/Controllers/RoomsController.cs
+++ src/TheGoodPlaceApi/TheGoodPlaceApi/Controllers/RoomsController.cs
@@ -22,6 +22,26 @@ namespace TheGoodPlaceApi.Controllers
             ranking.salles = ranking.salles.OrderByDescending(s => int.Parse(s.WellnessValue)).ToList();
             return Ok(ranking);
         }
+        [HttpGet("highest-room")]
+        [Produces("application/json", Type = null)]
+        public async Task<IActionResult> GetHighestRankingRoom()
+        {
+            var ranking = await _roomsServices.GetRoomRanking();
+            var highestRankingRoom = ranking.salles.OrderByDescending(s => int.Parse(s.WellnessValue)).FirstOrDefault();
+
+            return Ok(highestRankingRoom);
+        }
+        [HttpGet("other-rooms")]
+        [Produces("application/json", Type = null)]
+        public async Task<IActionResult> GetOtherRooms()
+        {
+            var ranking = await _roomsServices.GetRoomRanking();
+            var highestRankingRoom = ranking.salles.OrderByDescending(s => int.Parse(s.WellnessValue)).FirstOrDefault();
+            var otherRooms = ranking.salles.Where(s => s != highestRankingRoom).ToList();
+
+            return Ok(otherRooms);
+        }
+
 
 
     }
